d6b9a062f9446d0664e9ffbe030c862f338cf1e3
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2024-02-12 13:00:08 +0200

    database.js mukana

--- models/book_model.js
+++ models/book_model.js
@@ -1,14 +1,13 @@
+const db = require('../database.js');
+
 const book={
-    bookArray:[
-        {id_book:1, name:"Tietokannat", author:"Mikko Virtanen",isbn:"12345678" },
-        {id_book:2, name:"C++", author:"Liisa Virtanen",isbn:"333333333" },
-        {id_book:3, name:"JavaScript", author:"Matti Mainio",isbn:"888888888" }
-    ],
+
     getAllBooks(callback){
-        callback(this.bookArray);
+
+        return db.query('SELECT * FROM book',callback);
     },
     getOneBook(id, callback){
-        callback(this.bookArray[id]);
+        return db.query('SELECT * FROM book WHERE id_book=?',[id],callback);
     },
     addBook(insertData, callback){
         //tässä on SQL-injektio
--- routes/book.js
+++ routes/book.js
@@ -3,14 +3,26 @@ const router = express.Router();
 const book=require('../models/book_model');
 
 router.get('/',function(request,response){
-    book.getAllBooks(function(result){
-        response.json(result);
+    book.getAllBooks(function(err, result){
+        if(err){
+            console.log(err);
+            response.json(err.errno);
+        }
+        else{
+            response.json(result);
+        }
+        
     })
 });
 
 router.get('/:id',function(request, response){
-    book.getOneBook(request.params.id,function(result){
-        response.json(result);
+    book.getOneBook(request.params.id,function(err,result){
+        if(err){
+            response.json(err.errno);
+        }
+        else{
+            response.json(result[0]);
+        }
     })
 });
 
